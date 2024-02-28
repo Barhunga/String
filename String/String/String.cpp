ad3dfcc3015853c0d61da0d6c34650e04480213a
Barhunga <mm95x@hotmail.com>
2024-02-28 16:43:55 +1100

    first steps

--- String/String/String.cpp
+++ String/String/String.cpp
@@ -1,1 +1,19 @@
 #include "String.h"
+#include <iostream>
+using namespace std;
+
+String::String() { 
+
+}
+
+String::String(const char* _str) {
+
+}
+
+String::String(const String& _other) {
+
+}
+
+String::~String() {
+
+}
